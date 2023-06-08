02d5a89485dce5f9bb5f69d0a2a8dba7cdeaa257
T. Hamilton <profhamilton3@gmail.com>
2023-06-08 17:30:19 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+def on_received_number(receivedNumber):
+    if 1 == receivedNumber:
+        runRedBrickMotor(wuKong.MotorList.M2, True, 1)
+    elif 2 == receivedNumber:
+        runRedBrickMotor(wuKong.MotorList.M2, False, 1)
+    elif 3 == receivedNumber:
+        runRedBrickMotor(wuKong.MotorList.M1, True, 1)
+    elif 4 == receivedNumber:
+        runRedBrickMotor(wuKong.MotorList.M1, False, 1)
+    elif 5 == receivedNumber:
+        servos.P2.set_angle(0)
+    elif 6 == receivedNumber:
+        servos.P2.set_angle(60)
+    else:
+        pass
+radio.on_received_number(on_received_number)
+
 def runRedBrickMotor(motor: number, isLeft: bool, clicks: number):
     global lspeed
-    lspeed = 60
+    lspeed = 50
     if not (isLeft):
         lspeed = 0 - lspeed
     for index in range(clicks):
         wuKong.set_motor_speed(motor, lspeed)
-        basic.pause(200)
-        wuKong.stop_motor(motor)
+        basic.pause(100)
+    wuKong.stop_motor(motor)
 
 def on_button_pressed_a():
-    runRedBrickMotor(wuKong.MotorList.M1, False, 3)
+    servos.P2.set_angle(0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    runRedBrickMotor(wuKong.MotorList.M2, True, 2)
+    servos.P2.set_angle(60)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 lspeed = 0
-INIT = 0
-M1_POS = 0
-M2_POS = 0
-LEFT = 0
+
 wuKong.mecanum_wheel(wuKong.ServoList.S1,
     wuKong.ServoList.S2,
     wuKong.ServoList.S3,
@@ -32,9 +46,7 @@ servos.P2.set_angle(45)
 wuKong.stop_all_motor()
 radio.set_group(3)
 radio.set_frequency_band(33)
-basic.show_icon(IconNames.YES)
+basic.show_icon(IconNames.CONFUSED)
 radio.send_string("CLAW")
 MAX_CLICKS = 5
 MIN_CLICKS = -5
-motor_states = [-3, -2, -1, 0, 1, 2, 3]
-curMS = motor_states[INIT]
--- main.ts
+++ main.ts
@@ -1,39 +1,80 @@
-function runRedBrickMotor(motor: number, isLeft: boolean, clicks: number) {
-    
-    lspeed = 60
-    if (!isLeft) {
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Snake)
+    if (1 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        runRedBrickMotor(wuKong.MotorList.M2, true, 1)
+    } else if (2 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        runRedBrickMotor(wuKong.MotorList.M2, false, 1)
+    } else if (3 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        runRedBrickMotor(wuKong.MotorList.M1, true, 1)
+    } else if (4 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        runRedBrickMotor(wuKong.MotorList.M1, false, 1)
+    } else if (5 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        servos.P2.setAngle(15)
+    } else if (6 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        servos.P2.setAngle(75)
+    } else if (7 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+        wuKong.mecanumRun(wuKong.RunList.Front, 60)
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+    } else if (8 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 0, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+        wuKong.mecanumRun(wuKong.RunList.rear, 60)
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+    } else if (9 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 5000, 0, 255, 0, 500, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+        wuKong.mecanumSpin(wuKong.TurnList.Left, 35)
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+    } else if (10 == receivedNumber) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+        wuKong.mecanumSpin(wuKong.TurnList.Right, 35)
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+    } else if (11 == receivedNumber) {
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+        wuKong.mecanumRun(wuKong.RunList.stop, 0)
+    }
+})
+function runRedBrickMotor (motor: number, isLeft: boolean, clicks: number) {
+    lspeed = 50
+    if (!(isLeft)) {
         lspeed = 0 - lspeed
     }
-    
     for (let index = 0; index < clicks; index++) {
         wuKong.setMotorSpeed(motor, lspeed)
-        basic.pause(200)
-        wuKong.stopMotor(motor)
+basic.pause(100)
     }
+    wuKong.stopMotor(motor)
 }
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    runRedBrickMotor(wuKong.MotorList.M1, false, 3)
+input.onButtonPressed(Button.A, function () {
+    servos.P2.setAngle(15)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    runRedBrickMotor(wuKong.MotorList.M2, true, 2)
+input.onButtonPressed(Button.B, function () {
+    servos.P2.setAngle(75)
 })
 let lspeed = 0
-let INIT = 0
-let M1_POS = 0
-let M2_POS = 0
-let LEFT = 0
-wuKong.mecanumWheel(wuKong.ServoList.S1, wuKong.ServoList.S2, wuKong.ServoList.S3, wuKong.ServoList.S4)
+wuKong.mecanumWheel(
+wuKong.ServoList.S1,
+wuKong.ServoList.S2,
+wuKong.ServoList.S3,
+wuKong.ServoList.S4
+)
 wuKong.stopMotor(wuKong.MotorList.M1)
 wuKong.stopMotor(wuKong.MotorList.M2)
-servos.P2.setRange(0, 60)
-servos.P2.setAngle(45)
+servos.P2.setRange(15, 80)
+servos.P2.setAngle(15)
 wuKong.stopAllMotor()
+wuKong.setLightMode(wuKong.LightMode.BREATH)
 radio.setGroup(3)
 radio.setFrequencyBand(33)
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.Heart)
 radio.sendString("CLAW")
-let MAX_CLICKS = 5
-let MIN_CLICKS = -5
-let motor_states = [-3, -2, -1, 0, 1, 2, 3]
-let curMS = motor_states[INIT]
